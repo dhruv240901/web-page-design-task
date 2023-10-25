ede0aa0e1eb7dea7ba7e5f6b0b1652084b204846
Dhruv <dhruvp@zignuts.com>
2023-10-25 10:43:03 +0530

    modify inner content table

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,28 +35,33 @@
             <thead>
               <tr>
                 <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">email</th>
+                <th scope="col">Phone number</th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
+                <td>Dhruv</td>
+                <td>Patel</td>
+                <td>dhruv@gmail.com</td>
+                <td>4545454545</td>
               </tr>
               <tr>
                 <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
+                <td>Shubham</td>
+                <td>Patel</td>
+                <td>subham@gmail.com</td>
+                <td>3636363636</td>
               </tr>
               <tr>
                 <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
+                <td>Yash</td>
+                <td>Prajapati</td>
+                <td>yash@gmail.com</td>
+                <td>8888888888</td>
               </tr>
             </tbody>
           </table>
