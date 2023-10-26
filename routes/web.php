f07f3083d53a2605a975d98e88707856b0c31a15
Dhruv <dhruvp@zignuts.com>
2023-10-26 11:31:52 +0530

    add authentication middleware

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,15 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', [UserController::class,'index'])->name('index');
+Route::get('/', [HomeController::class,'index'])->name('index');
 Route::get('signup', [AuthController::class,'signup'])->name('signup');
 Route::post('signup', [AuthController::class,'CustomSignup'])->name('custom-signup');
 Route::get('login',[AuthController::class,'login'])->name('login');
 Route::post('login', [AuthController::class,'CustomLogin'])->name('custom-login');
 Route::get('logout', [AuthController::class,'logout'])->name('logout');
 
-Route::post('store-user',[UserController::class,'store'])->name('store-user');
-Route::post('update-user',[UserController::class,'update'])->name('update-user');
-Route::post('delete-user',[UserController::class,'destroy'])->name('delete-user');
+Route::group(['middleware' => ['auth']], function(){
+    Route::post('store-user',[UserController::class,'store'])->name('store-user');
+    Route::post('update-user',[UserController::class,'update'])->name('update-user');
+    Route::post('delete-user',[UserController::class,'destroy'])->name('delete-user');
+});
