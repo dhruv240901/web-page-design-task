4d2949041b0f0cb272b9108e4318212d8499238d
Dhruv <dhruvp@zignuts.com>
2023-10-27 09:46:33 +0530

    modify users table using migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('email')->unique();
+            $table->string('phone');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->unsignedBigInteger('owner_id')->nullable();
+            $table->foreign('owner_id')->references('id')->on('users')
+            ->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->enum('is_firsttime_login', [0,1])->default(0)->comment("0:'not firsttime login',1:'firsttime login'");
             $table->timestamps();
         });
     }
